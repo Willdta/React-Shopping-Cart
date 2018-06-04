989455c8570ac977fc37aad9d65d93ed7b67edde
Willdta <beyondutraining@gmail.com>
2018-06-04 11:49:52 -0400

    addItem function to add items to cart and keep track of price/remaining

--- src/App.js
+++ src/App.js
@@ -14,19 +14,45 @@ export default class App extends Component {
 
     items: {
       1: {
-        id: 1, name: 'Yeezys', price: 300, available: 5
+        id: 1, name: 'Yeezys', price: 300, remaining: 5
       },  
       
       2: {
-        id: 2, name: 'Github Sweater', price: 50, available: 10
+        id: 2, name: 'Github Sweater', price: 50, remaining: 10
       },
       
       3: {
-        id: 3, name: 'Protein Powder', price: 30, available: 20
+        id: 3, name: 'Protein Powder', price: 30, remaining: 20
       }
     },
 
     cartItems: []
+  }
+
+  addItem = item => {
+    const {
+      cart,
+      items,
+      cartItems
+    } = this.state
+
+    const cartItem = {
+      id: item.id,
+      name: item.name,
+      price: item.price
+    }
+
+    this.setState({
+      cart: {
+        ...cart,
+        ids: [...cart.ids, item.id],
+        quantity: {
+          ...cart.quantity,
+          [item.id]: cart.quantity[item.id] + 1
+        }
+      },
+      cartItems: [...cartItems, cartItem]
+    })
   }
 
   render() {
