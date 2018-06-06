062f2c9f8df7d3c2fca3fd1cb5a92dd3b89fac2b
Willdta <beyondutraining@gmail.com>
2018-06-06 11:29:46 -0400

    fixed total to display total of all items in shopping cart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import _ from 'lodash'
 
 export default class App extends Component {
   state = {
@@ -9,25 +10,25 @@ export default class App extends Component {
         1: 0, 
         2: 0, 
         3: 0
-      },
+      }
     },
 
     items: {
       1: {
-        id: 1, name: 'Yeezys', price: 300, remaining: 5, quantity: 0
+        id: 1, name: 'Yeezys', price: 50, remaining: 5, quantity: 1
       },  
       
       2: {
-        id: 2, name: 'Github Sweater', price: 50, remaining: 5, quantity: 0
+        id: 2, name: 'Github Sweater', price: 100, remaining: 5, quantity: 1
       },
       
       3: {
-        id: 3, name: 'Protein Powder', price: 30, remaining: 5, quantity: 0
+        id: 3, name: 'Protein Powder', price: 200, remaining: 5, quantity: 1
       }
     },
 
     total: 0,
-    removeQuantity: 0
+    editQuantity: 0
   }
 
   addItem = item => {
@@ -79,17 +80,17 @@ export default class App extends Component {
       total: total - (items[item.id].price * cart.quantity[item.id])
     })
   }
-
+  
   editQuantity = item => {
-    const { cart, items, removeQuantity } = this.state
-
-    if (removeQuantity > cart.quantity[item.id]) {
+    const { cart, items, total, editQuantity } = this.state
+    
+    if (editQuantity > cart.quantity[item.id]) {
       this.setState(prevState => ({
         items: {
           ...items,
           [item.id]: {
             ...item,
-            remaining: prevState.items[item.id].remaining - Math.abs(prevState.cart.quantity[item.id] - removeQuantity)
+            remaining: prevState.items[item.id].remaining - Math.abs(prevState.cart.quantity[item.id] - editQuantity)
           }
         }
       }))
@@ -99,28 +100,37 @@ export default class App extends Component {
           ...items,
           [item.id]: {
             ...item,
-            remaining: prevState.items[item.id].remaining + Math.abs(prevState.cart.quantity[item.id] - removeQuantity)
+            remaining: prevState.items[item.id].remaining + Math.abs(prevState.cart.quantity[item.id] - editQuantity)
           }
         }
       }))
     }
 
+    if (cart.quantity[item.id] < editQuantity) {
+      this.setState({
+        total: total + item.price * (Math.abs(cart.quantity[item.id] - editQuantity))
+      })    
+    } else {
+      this.setState({
+        total: total - item.price * (Math.abs(cart.quantity[item.id] - editQuantity))
+      }) 
+    }
+    
     this.setState({
       cart: {
         ...cart,
         quantity: {
           ...cart.quantity,
-          [item.id]: parseInt(removeQuantity, 10)
-        }
+          [item.id]: parseInt(editQuantity, 10)
+        },
       },
-      total: items[item.id].price * removeQuantity
     })
   }
 
-  handleChange = (e, item) => {
+  handleQuantityChange = (e, item) => {
     const { items } = this.state
 
-    let value = parseInt(e.target.value, 10)
+    const value = parseInt(e.target.value, 10)
 
     this.setState(prevState => ({
       items: {
@@ -134,8 +144,8 @@ export default class App extends Component {
   }
 
   render() {
-    const { items, cart, total,  } = this.state
-  
+    const { items, cart, total } = this.state
+
     return (
       <div className="App">
         <h1>Shopping Area</h1>
@@ -150,8 +160,8 @@ export default class App extends Component {
                 <p>Remaining: {item.remaining}</p>
                 <input 
                   type="number"
-                  value={ item.quantity }
-                  onChange={e => this.handleChange(e, item)}
+                  defaultValue={ item.quantity }
+                  onChange={e => this.handleQuantityChange(e, item)}
                   placeholder="quantity"
                   min={1}
                   max={5}
@@ -166,6 +176,8 @@ export default class App extends Component {
 
         <h1>Cart</h1>
 
+        { total > 0 && <h1>Total Price: ${total}</h1> }
+
         {cart.ids.length !== 0 ? Object.keys(items).map(id => (
           <div key={id}>
             {cart.quantity[id] > 0 && (
@@ -174,10 +186,10 @@ export default class App extends Component {
                 <p>Quantity: 
                   <input 
                     type="number"
-                    defaultValue={cart.quantity[id]}
-                    min="1"
-                    max="5"
-                    onChange={e => this.setState({ removeQuantity: parseInt(e.target.value, 10) })}
+                    defaultValue={items[id].quantity}
+                    min={1}
+                    max={5}
+                    onChange={e => this.setState({ editQuantity: parseInt(e.target.value, 10) })}
                     onBlur={() => this.editQuantity(items[id])}
                   />
                 </p>
@@ -187,8 +199,6 @@ export default class App extends Component {
             )}
           </div>
         )) : <h1>No Items In Your Cart</h1>}
-        
-        { total > 0 && <h1>Total Price: ${total}</h1> }
       </div>
     )
   }
