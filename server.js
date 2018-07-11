5bebb416ae4f3fc7533e753e478222ff516b6535
Willdta <beyondutraining@gmail.com>
2018-07-10 22:35:25 -0400

    d

--- server.js
+++ server.js
@@ -45,9 +45,9 @@ app.post('/sendMail', (req, res) => {
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error)
+    } else {
+      res.sendStatus(200)
     }
-
-    res.sendStatus(200)
   })
 })
 
