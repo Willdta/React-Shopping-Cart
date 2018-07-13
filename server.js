661b1182df020718124cfaa88734125516da3642
Willdta <beyondutraining@gmail.com>
2018-07-13 15:26:46 -0400

    added expiry

--- server.js
+++ server.js
@@ -45,7 +45,8 @@ app.post('/sendMail', (req, res) => {
       clientId: keys.clientID,
       clientSecret: keys.clientSecret,
       refreshToken: keys.refreshToken,
-      // accessToken: keys.accessToken
+      accessToken: keys.accessToken,
+      expires: 1484314697598
     }
   })
 
