91f8ddbb66bcfd8a31019c75102c947c326edac1
Willdta <beyondutraining@gmail.com>
2018-07-14 17:14:13 -0400

    testing

--- server.js
+++ server.js
@@ -57,11 +57,9 @@ app.post('/sendMail', (req, res) => {
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error)
-    } else {
-      console.log(info)
-    }
-    
-    // res.sendStatus(200)
+    } 
+      
+    res.sendStatus(200)
   })
 })
 
