86686411fcc98dae60221d18bde4f02778b81cd8
Willdta <beyondutraining@gmail.com>
2018-07-14 17:02:06 -0400

    git push

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer')
-const Oauth2 = require('oauth2')
 const keys = require('./config/keys')
 const port = process.env.PORT || 5000
 const app = express()
@@ -14,12 +13,11 @@ app.post('/sendMail', (req, res) => {
   const { name, email, address, city, province, postalCode, total } = req.body
   const allCapsPostal = postalCode.toUpperCase()
  
-  const capitalizer = word => {
-    return word
+  const capitalizer = word =>
+    word
       .split(' ')
       .map(word => word.charAt(0).toUpperCase() + word.slice(1))
       .join(' ')
-    }
   
   const output = `
     <p>Thanks for shopping with us, ${capitalizer(name)}.</p>
@@ -46,16 +44,7 @@ app.post('/sendMail', (req, res) => {
       clientId: keys.clientID,
       clientSecret: keys.clientSecret,
       refreshToken: keys.refreshToken,
-      accessToken: keys.accessToken
-    }
-  })
-
-  transporter.set('oauth2_provision_cb', (user, renew, callback) => {
-    let accessToken = userTokens[user];
-    if (!accessToken) {
-      return callback(new Error('Unknown user'));
-    } else {
-      return callback(null, accessToken);
+      // accessToken: keys.accessToken
     }
   })
 
@@ -65,8 +54,6 @@ app.post('/sendMail', (req, res) => {
     subject: 'Your Order',
     html: output
   }
-
-  transporter.on('token', token => console.log(token))
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
