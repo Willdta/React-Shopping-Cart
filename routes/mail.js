fe3483843f9f7fcb901f7748e22b05835aca10cd
Willdta <beyondutraining@gmail.com>
2018-07-16 10:51:56 -0400

    testing sending email with smtp

--- routes/mail.js
+++ routes/mail.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const nodemailer = require('nodemailer')
+const mail = require('nodemailer').mail
 const keys = require('../config/keys')
 const router = express.Router()
 
@@ -36,35 +36,43 @@ router.post('/sendMail', (req, res) => {
     <a href="https://reactshoppingcart1.herokuapp.com/">Shop</a>
   `
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      type: 'OAuth2',
-      user: 'beyondutraining@gmail.com',
-      clientId: keys.clientID,
-      clientSecret: keys.clientSecret,
-      refreshToken: keys.refreshToken,
-      // accessToken: keys.accessToken,
-      expires: 1000000000000000000
-    }
+  mail({
+    from: "Gabriel <beyondutraining@gmail.com>", 
+    to: email, 
+    subject: "Your Order", 
+    html: output 
   })
 
-  const mailOptions = {
-    from: '"Gabriel" <beyondutraining@gmail.com>',
-    to: email,
-    subject: 'Your Order',
-    html: output
-  }
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error)
-    }
+  // const transporter = nodemailer.createTransport({
+  //   host: 'smtp.gmail.com',
+  //   port: 465,
+  //   secure: true,
+  //   auth: {
+  //     type: 'OAuth2',
+  //     user: 'beyondutraining@gmail.com',
+  //     clientId: keys.clientID,
+  //     clientSecret: keys.clientSecret,
+  //     refreshToken: keys.refreshToken,
+  //     // accessToken: keys.accessToken,
+  //     expires: 1000000000000000000
+  //   }
+  // })
 
-    res.sendStatus(200)
-  })
+  // const mailOptions = {
+  //   from: '"Gabriel" <beyondutraining@gmail.com>',
+  //   to: email,
+  //   subject: 'Your Order',
+  //   html: output
+  // }
+
+  // transporter.sendMail(mailOptions, (error, info) => {
+  //   if (error) {
+  //     console.log(error)
+  //   }
+
+  //   res.sendStatus(200)
+  // })
 })
 
 module.exports = router
